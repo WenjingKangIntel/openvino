7eb2d117a44da9b0c67c86bb9dfdbfd2f195d0ca
Kang, Wenjing <wenjing.kang@intel.com>
2026-03-18 11:18:15 +0800

    Move VCL API to utils
    Signed-off-by: Kang, Wenjing <wenjing.kang@intel.com>

--- src/plugins/intel_npu/src/compiler_adapter/include/compiler_impl.hpp
+++ src/plugins/intel_npu/src/compiler_adapter/include/compiler_impl.hpp
@@ -7,7 +7,7 @@
 #include <memory>
 #include <optional>
 
-#include "compiler.h"
+#include "intel_npu/utils/vcl/vcl_api.hpp"
 #include "intel_npu/common/filtered_config.hpp"
 #include "intel_npu/network_metadata.hpp"
 #include "openvino/core/except.hpp"

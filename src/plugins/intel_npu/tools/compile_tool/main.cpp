e160f9ed8b1dde0a733a5a0061f657b4b300997b
Kang, Wenjing <wenjing.kang@intel.com>
2026-05-19 13:55:33 +0800

    Update compile_tool
    Signed-off-by: Kang, Wenjing <wenjing.kang@intel.com>

--- src/plugins/intel_npu/tools/compile_tool/main.cpp
+++ src/plugins/intel_npu/tools/compile_tool/main.cpp
@@ -502,8 +502,13 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        std::cout << "Compiling model" << std::endl;
+        std::cout << "Compiling model with CID" << std::endl;
+        configs["NPU_COMPILER_TYPE"] = "DRIVER";
         auto compiledModel = core.compile_model(model, FLAGS_d, {configs.begin(), configs.end()});
+        std::cout << "Compiling model with CIP" << std::endl;
+        configs["NPU_COMPILER_TYPE"] = "PLUGIN";
+        auto compiledModelCip = core.compile_model(model, FLAGS_d, {configs.begin(), configs.end()});
+
         loadNetworkTimeElapsed =
             std::chrono::duration_cast<TimeDiff>(std::chrono::steady_clock::now() - timeBeforeLoadNetwork);
         std::string outputName = FLAGS_o;
